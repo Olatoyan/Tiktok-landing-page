7a156f64ea55c1a5093f1fb2cd557379a42c9a17
Olatoyan <golatoyan@gmail.com>
2023-04-15 14:00:46 +0100

    updated file

--- script.js
+++ script.js
@@ -2,7 +2,41 @@
 
 const openMenuBtn = document.querySelector(".menu__open");
 const closeMenuBtn = document.querySelector(".menu__close");
-const menuNav = document.querySelector(".menu__links");
+const navBox = document.querySelector(".nav__box");
+
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
+
+openMenuBtn.addEventListener("click", function (e) {
+  console.log(e.target);
+  navBox.classList.add("nav__open");
+  // e.target.classList.add("close");
+  openMenuBtn.classList.add("close");
+  openMenuBtn.classList.remove("open");
+  closeMenuBtn.classList.add("open");
+  closeMenuBtn.classList.remove("close");
+});
+
+closeMenuBtn.addEventListener("click", function (e) {
+  console.log(e.target);
+
+  navBox.classList.remove("nav__open");
+  e.target.classList.add("close");
+  e.target.classList.remove("open");
+  openMenuBtn.classList.add("open");
+  openMenuBtn.classList.remove("close");
+});
+
+/*
+const openMenuBtn = document.querySelector(".menu__open");
+const closeMenuBtn = document.querySelector(".menu__close");
+const menuNav = document.querySelector(".nav__links");
 
 document.querySelector(".nav__links").addEventListener("click", function (e) {
   e.preventDefault();
@@ -16,16 +50,20 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
 // MENU NAVIGATION
 openMenuBtn.addEventListener("click", function () {
   menuNav.classList.add("visible");
-  menuNav.style.transform = "translateY(0%)";
+  menuNav.classList.add("nav-open");
+  // menuNav.style.transform = "translateX(0%)";
   closeMenuBtn.classList.add("menu--active");
 
-  openMenuBtn.classList.remove("menu--active");
+  openMenuBtn.classList.add("closed");
 });
 
 closeMenuBtn.addEventListener("click", function () {
   menuNav.classList.remove("visible");
-  menuNav.style.transform = "translateY(100%)";
+  menuNav.classList.remove("nav-open");
+
+  // menuNav.style.transform = "translateX(100%)";
   closeMenuBtn.classList.remove("menu--active");
 
-  openMenuBtn.classList.add("menu--active");
+  openMenuBtn.classList.remove("closed");
 });
+*/
